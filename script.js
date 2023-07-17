ff15d327d3a14716d45f11055625443c0315aca5
W.G. Kotzee <wgkotzee@gmail.com>
2023-07-17 21:29:01 +0200

    update my website

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-window.onload = function() {
-    alert("Welcome to My Website!");
-  };
 
-let inputDate = index.getElementById("input-date");
-let inputTime = index.getElementById("input-time");
-let countdownDisplay = index.getElementById("countdown");
-// Getting the user date and time input
-let userInputDate = new Date(inputDate.value + "T" + inputTime.value);
-// Gettint the current date and time
-const currentDate = new Date();
-// Calculate the difference
-const timeDiff = userInputDate.getTime() - currentDate.getTime();
+// Update the countdown every second
+const countdownTimer = setInterval(function() {
+  
+    // Get the input values for the date and time
+    const inputDate = document.getElementById("input-date").value;
+    const inputTime = document.getElementById("input-time").value;
 
-//Calculate all elements
-let seconds = Math.floor(timeDiff / 1000);
-let minutes = Math.floor(seconds / 60);
-let hours = Math.floor(minutes / 60);
-let days = Math.floor(hours / 24);
+    // Set the countdown date (replace with your desired date and time)
+    countdownDate = new Date(inputDate + " " + inputTime);
 
-//Calculate all remaining values
-hours = hours % 24;
-minutes = minutes % 60;
-seconds = seconds % 60;
+  // Get the current date and time
+  const currentDate = new Date();
 
-//Display the countdown
-index.getElementById("countdown").innerHTML = "Time remaining: "+ days + "days " + hours + "hours " + minutes + "minutes " + seconds + "seconds";
+  // Calculate the time between now and the countdown date
+  const timeDiff = countdownDate.getTime() - currentDate.getTime();
+
+  // Calculate the days, hours, minutes, and seconds
+  const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+  // Display the countdown
+  document.getElementById("countdown").innerHTML = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+
+  // If the countdown is finished, display a message
+  if (timeDiff < 0) {
+    clearInterval(countdownTimer);
+    document.getElementById("countdown").innerHTML = "Event has started!";
+  }
+}, 1000);
